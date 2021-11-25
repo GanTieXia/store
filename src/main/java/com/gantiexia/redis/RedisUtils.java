b78834cb1d2ca89ef8ccb0d9cc0d9fb9e028bdf3
me <1336024089@qq.com>
2021-11-25 20:46:18 +0800

    优化

--- src/main/java/com/gantiexia/redis/RedisUtils.java
+++ src/main/java/com/gantiexia/redis/RedisUtils.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 /**
